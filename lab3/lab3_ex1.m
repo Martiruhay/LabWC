fb19f0a7f6741831fce98c61165ad0ad0f2fc4ce
wextia <valcazar96@gmail.com>
2017-10-17 17:49:58 +0200

    Mean filter done

--- lab3/lab3_ex1.m
+++ lab3/lab3_ex1.m
@@ -1,16 +1,18 @@
 function [out] = lab3_ex1(in)
-% efficient median filter
-% [1 2 current 4 5]
-% current = avg[1 2 current 4 5]
+% efficient average filter
 out = in;
+offset = 2;
 [r,c] = size(in);
-for i = offset+1:(r-offset)
-    for j = offset+1:(c-offset)
-        val = in(i,j);
-        % [1 2 3 4 5] / 5
-        % 1 ([2 3 4 5 6] - 1) / 5
-        
-        
+
+col_array = in(1,1:5);
+
+for i = offset + 1: (r-offset)
+    for j = offset + 1: (c-offset)
+        a = mean(col_array);
+        out(i,j) = a;
+        if(j < c - offset)
+            col_array = [col_array(2:end), in(i,j + 3)];
+        end
     end
 end
 end
